b17eb8249572dd6bed186ea2d1134f608a2b2cb4
Samsoucy <newf930@outlook.com>
2019-03-08 13:59:14 -0500

    changes register and login functions. Made changes to file to make it work. Added authentication code

--- api/server.js
+++ api/server.js
@@ -4,13 +4,15 @@ const helmet = require('helmet');
 
 const configureRoutes = require('../config/routes.js');
 
+
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-server.use('/api/config', configureRoutes);
 
-// configureRoutes(server);
+
+
+configureRoutes(server);
 
 module.exports = server;
--- config/routes.js
+++ config/routes.js
@@ -1,82 +1,71 @@
-const axios = require('axios');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const db = require("../database/dbConfig");
 
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 
-const { authenticate } = require('../auth/authenticate');
-const Users = require('../users/users-model.js');
+const {authenticate, generateToken} = require("../auth/authenticate");
+
+
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
-//***********creates a new user*************/
 
-router.post('/register', (req, res) => {
-  let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10); 
-  user.password = hash;
 
-  Users.add(user)
-    .then(saved => {
-      res.status(201).json(saved);
+
+function register(req, res) {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 10);
+  creds.password = hash;
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      const token = generateToken(creds);
+      res.status(201).json({
+        ids,
+        message: "registered",
+        token
+      });
     })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
+    .catch(err => res.status(500).json(err));
+}
 
-//***********login user*************/
-
-router.post('/login', (req, res) => {
-  let { username, password } = req.body;
-
-  Users.findBy({ username })
+function login(req, res) {
+  const creds = req.body;
+  db("users")
+    .where({ username: creds.username })
     .first()
     .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        const token = generateToken(user); 
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
         res.status(200).json({
-          message: `Welcome ${user.username}!, have a token...`,
-          token, 
+          message: `Welcome ${
+            user.username
+          }here's a cookie and a token`,
+          token
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "You shall not pass!" });
       }
     })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
-
-function generateToken(user) {
-  const payload = {
-    subject: user.id,
-    username: user.username,
-    };
-    const options = {
-    expiresIn: '1d',
-  };
-  return jwt.sign(payload, secrets.jwtSecret, options);
-};
-
+    .catch(err => console.log(err));
+}
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
-
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
 
-module.exports = router;
+
